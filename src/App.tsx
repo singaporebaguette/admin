513818f626d33967293d14c4e191682d0f5abfef
Aksels Ledins <aksels.ledins@gmail.com>
2020-05-13 21:43:26 +0800

    allow access to certain users

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,34 @@ import stores from './entities/stores';
 import { Admin, Resource } from 'react-admin';
 import { FirebaseDataProvider, FirebaseAuthProvider, RAFirebaseOptions } from 'react-admin-firebase';
 import Login from './Login';
-
+import approvedUsers from './approvedUsers';
 const config = require('./fconfig.js').firebaseConfig;
 
 const options: RAFirebaseOptions = {
-  logging: true,
+  logging: false,
   rootRef: '/',
   watch: ['stores'],
 };
 const dataProvider = FirebaseDataProvider(config, options);
 const authProvider = FirebaseAuthProvider(config, options);
 
+const ap = {
+  ...authProvider,
+  checkAuth: async (params: any) => {
+    const user: any = await authProvider.checkAuth();
+
+    if (!approvedUsers.includes(user.uid)) {
+      throw new Error('not authorized sorry');
+    }
+
+    return user;
+  },
+};
+
 class App extends React.Component {
   render() {
     return (
-      <Admin dataProvider={dataProvider} authProvider={authProvider} loginPage={Login}>
+      <Admin dataProvider={dataProvider} authProvider={ap} loginPage={Login}>
         <Resource name="stores" list={stores.List} create={stores.Create} edit={stores.Edit} show={stores.Show} />
       </Admin>
     );
---
+++ src/approvedUsers.ts
@@ -0,0 +1,1 @@
+export default ['ETWYO4lPKSSq5IclOSvha2etFz82', 'teBxhoPLshQh8DO38ssoWXNms6h2'];
